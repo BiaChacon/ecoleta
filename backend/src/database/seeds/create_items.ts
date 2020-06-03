0cc70feec04bf5b99523e7508b9d516520d219a9
biachacon <anabheatrizchacon@outlook.com>
2020-06-03 17:22:10 -0300

    .

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
     await knex('items').insert([
         {title: 'Lâmpada', image: 'lampadas.svg'},
         {title: 'Pilhas e Baterias', image: 'baterias.svg'},
-        {title: 'Papéis e Papelão', image: 'papaeis-papelao.svg'},
+        {title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
         {title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
         {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
         {title: 'Óleo de cozinha', image: 'oleo.svg'}
